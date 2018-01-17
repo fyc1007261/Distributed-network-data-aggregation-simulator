11e35a612b8feaa19818797ca4d171221e66454c
Yifan Cai <fyc1007261@sjtu.edu.cn>
2018-01-17 15:43:23 +0800

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -4,12 +4,14 @@ import random
 
 def main():
     size = 66
-    net = Network(size)
+    net = Network(True, size)
     data = []
     flags = []
     for i in range(size):
         data.append(i)
     net.set_data(data)
+    net.save_topology()
     print(net.pdf_aggregation())
+
 
 main()
