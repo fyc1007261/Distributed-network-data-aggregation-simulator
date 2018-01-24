31a573e3c2935fd853960a038bce5d169e84b390
Yifan Cai <fyc1007261@sjtu.edu.cn>
2018-01-24 14:22:21 +0800

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -2,26 +2,27 @@ from network import *
 import random
 
 
-def read_data(name):
-    f = open(name, 'r')
+def read_data(file_name):
+    f = open(file_name, 'r')
     s = f.readline()
     return eval(s)
 
 
 def main():
-    size = 166
-    net = Network(size)
-    data = []
     size = 100
-    net = Network(True, size)
+    net = Network(new_top=False, num=size, topo_file="topology.txt")
     data = read_data("data.txt")
     flags = []
     net.set_data(data)
-    print(net.calculate_avg(net.value))
-    #print(net.pdf_aggregation())
-    print(multiply(net.pdf_aggregation_without_id(10)[:,0], size))
-    net.save_topology()
-    print(net.pdf_aggregation())
+    #print(net.id_based_pdf_aggregation())
+    print(net.generic_pdf_consensus(sections=20, sim=True).T[0])
+    #print(net.generic_pdf_consensus(sections=40, sim=True).T[0])
+    #print(net.generic_pdf_consensus(sections=100, sim=True).T[0])
+    # Network 2
+    net2 = Network(new_top=False, num=size, topo_file="topology2.txt")
+    net2.set_data(data)
+    print(net2.generic_pdf_consensus(sections=20, sim=True).T[0])
+    plt.show()
 
 
 main()
